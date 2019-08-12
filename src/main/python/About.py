c1f99f4849b1ce2ed488b0b3e0a0607571fe25b2
CodingMan <thetruth0906@gmail.com>
2019-08-12 13:51:44 +0800

    Add about font

--- src/main/python/About.py
+++ src/main/python/About.py
@@ -15,16 +15,16 @@ import Ver
 class Ui(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(359, 189)
-        Dialog.setMinimumSize(QtCore.QSize(0, 0))
-        Dialog.setMaximumSize(QtCore.QSize(400, 300))
+        Dialog.resize(590, 340)
+        Dialog.setMinimumSize(QtCore.QSize(590, 340))
+        Dialog.setMaximumSize(QtCore.QSize(590, 340))
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(50, 150, 261, 32))
+        self.buttonBox.setGeometry(QtCore.QRect(300, 290, 261, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(200, 20, 151, 111))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(350, 20, 221, 121))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -44,7 +44,7 @@ class Ui(object):
         self.SourceCode.setObjectName("SourceCode")
         self.verticalLayout.addWidget(self.SourceCode)
         self.IconDisplay = QtWidgets.QLabel(Dialog)
-        self.IconDisplay.setGeometry(QtCore.QRect(20, 20, 150, 150))
+        self.IconDisplay.setGeometry(QtCore.QRect(20, 20, 300, 300))
         self.IconDisplay.setObjectName("IconDisplay")
 
         self.retranslateUi(Dialog)
@@ -56,21 +56,34 @@ class Ui(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowIcon(QIcon('./src/res/Small.PNG'))
         Dialog.setWindowTitle(_translate("Dialog", " 關於 PTT Postman"))
+
+        Font = QtGui.QFont('微軟正黑體', 14, QtGui.QFont.Bold) 
+
         self.Version.setText(_translate("Dialog", "PTT Postman v " + Ver.V))
+        self.Version.setFont(Font)
+
         self.Developer.setText(_translate(
-            "Dialog", "Developer: PTT CodingMan"))
+            "Dialog", "Developer: CodingMan"))
+        self.Developer.setFont(Font)
+
         self.Issue.setText(_translate(
             "Dialog", "<a href=\"https://github.com/Truth0906/PTTPostman/issues/new\">回報問題</a>"))
         self.Issue.setOpenExternalLinks(True)
+        self.Issue.setFont(Font)
+
         self.SourceCode.setText(_translate(
             "Dialog", "<a href=\"https://github.com/Truth0906/PTTPostman\">原始碼</a>"))
         self.SourceCode.setOpenExternalLinks(True)
+        self.SourceCode.setFont(Font)
+
         self.IconDisplay.setText(_translate("Dialog", "TextLabel"))
 
         IconImage = QPixmap('./src/res/OriginIcon.PNG')
-        IconImage = IconImage.scaled(150, 150, QtCore.Qt.KeepAspectRatio)
+        IconImage = IconImage.scaled(300, 300, QtCore.Qt.KeepAspectRatio)
         self.IconDisplay.setPixmap(IconImage)
 
+        
+        
 
 def start(app):
 
@@ -89,3 +102,4 @@ if __name__ == "__main__":
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
+
