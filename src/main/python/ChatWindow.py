48fab4327ada61fb4c88d6a53c1e38b9a52cf793
CodingMan <thetruth0906@gmail.com>
2019-08-25 21:47:14 +0800

    Scroll area first time success, WIP

--- src/main/python/ChatWindow.py
+++ src/main/python/ChatWindow.py
@@ -10,46 +10,62 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
+    def setupUi(self, Dialog, Target):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(508, 382)
+        Dialog.resize(300, 500)
         self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.scrollArea = QtWidgets.QScrollArea(Dialog)
+        self.scrollArea.setEnabled(True)
+        self.scrollArea.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.scrollArea.setLineWidth(0)
+        self.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 486, 329))
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 298, 430))
+        self.scrollAreaWidgetContents.setMinimumSize(QtCore.QSize(0, 0))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.verticalLayout.addWidget(self.scrollArea)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setMinimumSize(QtCore.QSize(0, 60))
+        self.lineEdit.setFrame(False)
+        self.lineEdit.setAlignment(
+            QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
         self.lineEdit.setObjectName("lineEdit")
         self.horizontalLayout.addWidget(self.lineEdit)
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
-        self.retranslateUi(Dialog)
+        self.retranslateUi(Dialog, Target)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, Dialog, Target):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "PushButton"))
+        Dialog.setWindowTitle(_translate("Dialog", Target))
+
+        inner = QtWidgets.QFrame(self.scrollArea)
+        inner.setLayout(QtWidgets.QVBoxLayout())
+
+        self.scrollArea.setWidget(inner)
+
+        for i in range(40):
+            b = QtWidgets.QLabel(inner)
+            b.setText(str(i))
+            inner.layout().addWidget(b)
 
 
-def start(ConfigObj):
+def start(ConfigObj, Target):
 
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
+    ui.setupUi(Dialog, Target)
     Dialog.show()
     Dialog.exec()
 
@@ -62,3 +78,4 @@ if __name__ == "__main__":
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
+
