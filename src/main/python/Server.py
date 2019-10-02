9408f018c49c808f5a3e567b2f37c117e1d720f0
CodingMan <CodingMan@uptt.cc>
2019-10-02 23:37:39 +0800

    嘗試建立最棒的伺服器

--- src/main/python/Server.py
+++ src/main/python/Server.py
@@ -8,7 +8,7 @@ PortList = [
     49165, 50730, 61512
 ]
 
-
+# Type 1
 async def handler(websocket, path):
     print(path)
     while True:
@@ -22,13 +22,14 @@ async def handler(websocket, path):
             print('連線已經關閉')
             return
         print(f"< {Msg}")
-
+        if Msg == 'close':
+            return
         Response = f"Echo [{Msg}]"
         print(f"> {Response}")
 
         await websocket.send(Response)
-        await websocket.send(Response)
 
+# type 2
 # async def receiver(message):
 #     print(f'receive [{message}]')
 
@@ -45,17 +46,52 @@ async def handler(websocket, path):
 #         if message is not None:
 #             await websocket.send(message)
 
+# @asyncio.coroutine
 # async def handler(websocket, path):
 #     consumer_task = asyncio.ensure_future(
-#         receiver_handler(websocket, path))
+#         receiver_handler(websocket, path)
+#     )
 #     producer_task = asyncio.ensure_future(
-#         producer_handler(websocket, path))
-#     done, pending = await asyncio.wait(
+#         producer_handler(websocket, path)
+#     )
+#     _, pending = await asyncio.wait(
 #         [consumer_task, producer_task],
 #         return_when=asyncio.FIRST_COMPLETED,
 #     )
 #     for task in pending:
 #         task.cancel()
+    
+#     print('Finish')
+
+# Type 3
+# connected = set()
+
+# async def handler(websocket, path):
+#     # Register.
+#     connected.add(websocket)
+#     try:
+#         # Implement logic here.
+#         # await asyncio.wait([ws.send("Hello!") for ws in connected])
+
+#         print(path)
+#         while True:
+#             try:
+#                 print('準備接收')
+#                 Msg = await websocket.recv()
+#             except websockets.exceptions.ConnectionClosedError:
+#                 print('連線已經關閉')
+#                 return
+#             except websockets.exceptions.ConnectionClosedOK:
+#                 print('連線已經關閉')
+#                 return
+#             print(f"< {Msg}")
+#             if Msg == 'close':
+#                 return
+#             Response = f"Echo [{Msg}]"
+#             print(f"> {Response}")
+#     finally:
+#         # Unregister.
+#         connected.remove(websocket)
 
 # https://websockets.readthedocs.io/en/stable/intro.html
 for i in range(len(PortList)):
