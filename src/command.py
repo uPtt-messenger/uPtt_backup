759478d2618f0203a70a37e630ae590399167f94
CodingMan <CodingMan@uptt.cc>
2019-12-21 15:42:17 +0800

    echo api test success

--- src/command.py
+++ src/command.py
@@ -1,4 +1,12 @@
 
+import json
+
+from errorcode import ErrorCode
+
+Key_Opt = 'operation'
+Key_Msg = 'msg'
+Key_Code = 'code'
+
 
 class Command:
     def __init__(self):
@@ -7,8 +15,18 @@ class Command:
 
         self.PushMsg = []
 
-    def analyze(self, RecvMsg):
-        pass
+    def analyze(self, RecvMsg: str):
+        Msg = json.loads(RecvMsg)
+
+        if Msg[Key_Opt] == 'echo':
+
+            ResMsg = dict()
+            ResMsg[Key_Code] = ErrorCode.Success
+            ResMsg[Key_Msg] = Msg[Key_Msg]
+
+            self.PushMsg.append(
+                json.dumps(ResMsg)
+            )
 
     def push(self, PushMsg):
 
