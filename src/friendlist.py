3eeac61a9674f6368ac35734189f7cc7429f7cb6
CodingMan <CodingMan@uptt.cc>
2020-01-08 17:39:03 +0800

    add friendlist

---
+++ src/friendlist.py
@@ -0,0 +1,15 @@
+
+import log
+
+
+class FriendList:
+    def __init__(self):
+        self.friendlist = []
+
+    def add(self, target):
+        self.friendlist.append(target)
+
+    def remove(self, target):
+        if target in self.friendlist:
+            self.friendlist.remove(target)
+
