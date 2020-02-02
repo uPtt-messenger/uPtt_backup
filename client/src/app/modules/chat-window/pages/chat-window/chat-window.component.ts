eca7b9fe10352a180d125e817a76552def086f38
Doreen-PC\Administrator <doreen761003@gmail.com>
2020-02-03 01:01:57 +0800

    Auto scroll to bottom when message box overflow.

--- client/src/app/modules/chat-window/pages/chat-window/chat-window.component.ts
+++ client/src/app/modules/chat-window/pages/chat-window/chat-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { ChatMessage } from 'src/app/modules/shared/models/chat-message';
 
@@ -7,7 +7,9 @@ import { ChatMessage } from 'src/app/modules/shared/models/chat-message';
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.scss']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, AfterViewChecked {
+
+  @ViewChild('messageBox', {static: true}) private messageBoxRef: ElementRef;
 
   chatMsgList: ChatMessage[] = [
     { msgType: 2, msgTime: '2020/02/02 01:16:25', sender: 'CodingMan', msgContent: '測試測試測試測試' },
@@ -30,7 +32,11 @@ export class ChatWindowComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.scrollToBottom();
+  }
 
+  ngAfterViewChecked() {
+    this.scrollToBottom();
   }
 
   submitMsg(event) {
@@ -41,6 +47,12 @@ export class ChatWindowComponent implements OnInit {
       this.chatMsgList.push({ msgType: 1, msgTime: '2020/02/03 00:30:16', sender: 'mobi76', msgContent: chatMsg });
       this.chatForm.reset();
     }
+  }
+
+  scrollToBottom(): void {
+    try {
+        this.messageBoxRef.nativeElement.scrollTop = this.messageBoxRef.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 
 }
