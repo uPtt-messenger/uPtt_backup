3d1b66b25dfeaacbe5471ed52b28b669a00cafc4
Doreen-PC\Administrator <doreen761003@gmail.com>
2020-02-02 23:42:17 +0800

    Add new model ChatMessage.

--- client/src/app/modules/chat-window/pages/chat-window/chat-window.component.ts
+++ client/src/app/modules/chat-window/pages/chat-window/chat-window.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { ChatMessage } from 'src/app/modules/shared/models/chat-message';
 
 @Component({
   selector: 'app-chat-window',
@@ -7,6 +8,14 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./chat-window.component.scss']
 })
 export class ChatWindowComponent implements OnInit {
+
+  chatMsgList: ChatMessage[] = [
+    { msgType: 2, msgTime: '2020/02/02 01:16:25', sender: 'CodingMan', msgContent: '測試測試測試測試' },
+    { msgType: 1, msgTime: '2020/02/02 01:16:35', sender: 'mobi76', msgContent: '測試測試測試測試測試測試測試測試測試測試' },
+    { msgType: 1, msgTime: '2020/02/02 01:16:52', sender: 'mobi76', msgContent: '測試測試測試' },
+    { msgType: 2, msgTime: '2020/02/02 01:17:01', sender: 'CodingMan', msgContent: '測試測試測試測試' },
+    { msgType: 1, msgTime: '2020/02/02 01:17:16', sender: 'mobi76', msgContent: '測試測試測試測試測試測試測試' }
+  ];
 
   chatMsgLen = 0;
   chatForm: FormGroup;
---
+++ client/src/app/modules/shared/models/chat-message.ts
@@ -0,0 +1,6 @@
+export class ChatMessage {
+  msgType: number;
+  msgTime: string;
+  sender: string;
+  msgContent: string;
+}
