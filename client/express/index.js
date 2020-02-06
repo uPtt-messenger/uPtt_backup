cae41a2ad212e82d7e8565ddc5968d3997c0866a
Doreen-PC\Administrator <doreen761003@gmail.com>
2020-02-06 23:39:42 +0800

    Complete mock server login API.

--- client/express/index.js
+++ client/express/index.js
@@ -3,11 +3,10 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 
 // 指定開啟的 port
-const PORT = 3000;
+const PORT = 50732;
 
 // 創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
-const server = express()
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // 將 express 交給 SocketServer 開啟 WebSocket 的服務
 const wss = new SocketServer({ server });
@@ -15,11 +14,27 @@ const wss = new SocketServer({ server });
 // 當 WebSocket 從外部連結時執行
 wss.on('connection', ws => {
 
-    // 連結時執行此 console 提示
-    console.log('Client connected')
+  // 連結時執行此 console 提示
+  console.log('[' + new Date().toUTCString() + '] Client connected');
 
-    // 當 WebSocket 的連線關閉時執行
-    ws.on('close', () => {
-        console.log('Close connected')
-    });
+  ws.on('message', reqDataString => {
+    console.log('[' + new Date().toUTCString() + '] ========= Request ========= ');
+    const reqData = JSON.parse(reqDataString);
+    console.log('[' + new Date().toUTCString() + '] operation = ' + reqData.operation);
+    console.log('[' + new Date().toUTCString() + '] payload = ' + JSON.stringify(reqData.payload));
+
+    // 登入
+    if (reqData.operation === 'login') {
+      const resp = {code: 0, payload: "login success"};
+      console.log('[' + new Date().toUTCString() + '] ========= Response ========= ');
+      console.log('[' + new Date().toUTCString() + '] resp = ' + JSON.stringify(resp));
+      ws.send(JSON.stringify(resp));
+    }
+
+  });
+
+  // 當 WebSocket 的連線關閉時執行
+  ws.on('close', () => {
+      console.log('Close connected')
+  });
 });
