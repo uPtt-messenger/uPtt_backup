a9cc576df5fba890f04fd137e8c62ceeff59429b
CodingMan <CodingMan@uptt.cc>
2020-02-24 17:58:12 +0800

    feedback, WIP

--- server/src/feedback.py
+++ server/src/feedback.py
@@ -1,18 +1,28 @@
-
 import threading
-
+import time
 from msg import Msg
+
 
 class Feedback:
     def __init__(self, console_obj):
         self.console = console_obj
+
+        self.close = False
+        self.closed = False
+
         self.thread = threading.Thread(target=self.run)
         self.thread.start()
 
+    def event_close(self):
+        self.close = True
+
+        while True:
+            if self.closed:
+                break
+            time.sleep(0.5)
+
     def run(self):
+        while not self.close:
+            
 
-        pass
-
-        # while not self.config.command.close:
-        #
-        #     while
+        self.closed = True
