a474071456551e51b6c6a1840c65936f6ef905e4
CodingMan <CodingMan@uptt.cc>
2020-02-26 09:53:33 +0800

    Add tag func

---
+++ server/tag/tag.py
@@ -0,0 +1,10 @@
+
+
+import hashlib
+
+def sha256(s):
+    s_lower = s.lower()
+    hash = hashlib.sha256(s_lower.encode('utf-8')).hexdigest()
+    print(f'[{s}]\n[{s_lower}]\n[{hash}]')
+
+sha256('mobi76')
