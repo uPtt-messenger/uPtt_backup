2b13f53d2fec500a71876dcf45e28945ba2e7980
CodingMan <CodingMan@uptt.cc>
2020-02-26 16:34:39 +0800

    add black list in dynamic data

--- server/src/dynamic_data.py
+++ server/src/dynamic_data.py
@@ -19,3 +19,18 @@ class DynamicData:
                 'tag',
                 tag
             )
+
+        with urllib.request.urlopen(
+                "https://raw.githubusercontent.com/PttCodingMan/uPtt_open_data/master/list/blacklist.json") as url:
+            self.black_list = json.loads(url.read().decode())
+
+        for block_user in self.black_list:
+            if block_user.startswith('//'):
+                continue
+            log.show_value(
+                'DynamicData',
+                log.level.INFO,
+                'block_user',
+                block_user
+            )
+
