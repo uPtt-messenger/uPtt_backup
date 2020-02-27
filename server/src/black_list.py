d825ff9f8c0b98d9111359f2a5f8a7c0110155d6
CodingMan <CodingMan@uptt.cc>
2020-02-27 11:06:51 +0800

    Fixed black list error

--- server/src/black_list.py
+++ server/src/black_list.py
@@ -1,7 +1,7 @@
 from util import sha256
 
 
-def is_black_user(console_obj, ptt_id):
+def is_black_user(dynamic_data, ptt_id):
     current_hash_value = sha256(ptt_id)
 
-    return current_hash_value in console_obj.dynamic_data.black_list
+    return current_hash_value in dynamic_data.black_list
