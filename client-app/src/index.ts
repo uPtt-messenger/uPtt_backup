852552d42d281b47cea7ca59814214bbbd758c4a
Doreen-PC\Administrator <doreen761003@gmail.com>
2020-03-19 23:51:43 +0800

    [client-app] Adjust index to typescript.

---
+++ client-app/src/index.ts
@@ -0,0 +1,10 @@
+import { App } from './app';
+import { LogManager } from './lib/log-manager';
+import { ConfigManager } from './lib/config-manager';
+import { WindowManager } from './lib/window-manager';
+
+const logger = new LogManager();
+const configManager = new ConfigManager();
+const windowManager = new WindowManager(configManager);
+
+const upttClient = new App(logger, configManager, windowManager);
