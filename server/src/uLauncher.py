e0e4a9fb50a022fd522e469750e894bea64c52f6
CodingMan <CodingMan@uptt.cc>
2020-03-25 09:51:25 +0800

    Add launch uPtt

--- server/src/uLauncher.py
+++ server/src/uLauncher.py
@@ -1,3 +1,4 @@
+import os
 from io import BytesIO
 import zipfile
 from urllib.request import urlopen
@@ -52,6 +53,4 @@ if version_compare_result < 0 or not file_exist:
     zip_file = zipfile.ZipFile(BytesIO(resp.read()))
     zip_file.extractall(path=config_obj.config_path)
 
-# resp = urlopen(url)
-# zipfile = ZipFile(BytesIO(resp.read()))
-# print(zipfile.namelist())
+os.startfile(f'{config_obj.config_path}/uPtt.exe')
