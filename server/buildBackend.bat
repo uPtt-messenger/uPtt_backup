4841432079724a0bd50bec949b26c6edc4ea48ac
CodingMan <CodingMan@uptt.cc>
2020-03-27 09:29:24 +0800

    Adjust build script

--- server/buildBackend.bat
+++ server/buildBackend.bat
@@ -1,28 +1,38 @@
 @echo off
 cls
 
-cd dist
 del /f uPtt.exe
-cd ..
 
 rem nuitka --windows-disable-console --recurse-all src/uPtt.py
 
 rem pyinstaller -w --onefile --icon="..\client\src\assets\images\uptt.ico" .\src\uPtt.py
-pyinstaller -w --onefile .\src\uPtt.py
+rem pyinstaller -w --onefile .\src\uPtt.py
 rem pyinstaller -w --onefile .\src\uLauncher.py
+nuitka --windows-disable-console --recurse-all src/uPtt.py
 
-IF EXIST "dist\uPtt.exe" (
+IF EXIST "uPtt.exe" (
   REM Do one thing
 ) ELSE (
     echo Build error
     goto end
 )
 
+nuitka --windows-disable-console --recurse-all src/uLauncher.py
+
+IF EXIST "uLauncher.exe" (
+  REM Do one thing
+) ELSE (
+    echo Build error
+    goto end
+)
+
+
 cd dist
 python zip.py
 cd ..
 
-copy dist\uPtt.zip package
+move uPtt.zip package
+move uLauncher.zip package
 
 :end
 echo Finish
