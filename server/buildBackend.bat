929cce64d97d832f4be42036c0d1cb9ad0019fec
CodingMan <CodingMan@uptt.cc>
2020-03-28 20:14:42 +0800

    Adjust build script

--- server/buildBackend.bat
+++ server/buildBackend.bat
@@ -9,25 +9,24 @@ rem nuitka --windows-disable-console --recurse-all src/uPtt.py
 rem pyinstaller -w --onefile --icon="..\client\src\assets\images\uptt.ico" .\src\uPtt.py
 rem pyinstaller -w --onefile .\src\uPtt.py
 rem pyinstaller -w --onefile .\src\uLauncher.py
+echo Build uPtt [Active]
 nuitka --windows-disable-console --recurse-all src/uPtt.py
+echo Build uPtt [Complete]
 
-IF EXIST "uPtt.exe" (
-  REM Do one thing
-) ELSE (
-    echo Build error
-    goto end
-)
+echo Sign uPtt [Active]
+signtool sign /a /tr http://timestamp.comodoca.com uPtt.exe
+echo Sign uPtt [Complete]
 
+echo Sign uLauncher [Active]
 nuitka --windows-disable-console --recurse-all src/uLauncher.py
-signtool sign /a /tr http://timestamp.comodoca.com uLauncher.exe
+echo Build uLauncher [Complete]
+rem signtool sign /t http://timestamp.digicert.com /sha1 25eb809119b4052ebcbc106d163b6d5ff83116bb uLauncher.exe
+rem signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /as /sha1 25eb809119b4052ebcbc106d163b6d5ff83116bb uLauncher.exe
 rem signtool sign /as /tr http://timestamp.comodoca.com /td sha256 /fd sha256 uLauncher.exe
 
-IF EXIST "uLauncher.exe" (
-  REM Do one thing
-) ELSE (
-    echo Build error
-    goto end
-)
+echo Sign uLauncher [Active]
+signtool sign /a /tr http://timestamp.comodoca.com uLauncher.exe
+echo Sign uLauncher [Complete]
 
 python zip.py
 
