7a48b21148cbb25c4cc1bd0fdbafecc957a02846
CodingMan <CodingMan@uptt.cc>
2020-03-29 19:29:00 +0800

    Adjust uPtt to core, adjust uLauncher to uPtt

---
+++ server/src/core.py
@@ -0,0 +1,130 @@
+import os
+import sys
+import time
+import threading
+
+import log
+import websocketserver
+from config import Config
+from command import Command
+from pttadapter import PTT_Adapter
+from feedback import Feedback
+from event import EventConsole
+from console import Console
+from dynamic_data import DynamicData
+from black_list import BlackList
+
+LogPath = None
+
+
+def log_to_file(msg):
+    global LogPath
+    if LogPath is None:
+        desktop = os.path.join(
+            os.path.join(
+                os.environ['USERPROFILE']
+            ),
+            'Desktop'
+        )
+
+        LogPath = f'{desktop}/uPttLog.txt'
+
+        print(LogPath)
+
+    with open(LogPath, 'a', encoding='utf8') as f:
+        f.write(f'{msg}\n')
+
+
+if __name__ == '__main__':
+
+    config_obj = Config()
+
+    console_obj = Console()
+    console_obj.config = config_obj
+
+    log.show_value(
+        'Main',
+        log.level.INFO,
+        'uPtt 版本',
+        config_obj.version
+    )
+
+    if len(sys.argv) > 1:
+        print(sys.argv)
+
+    if '-debug' in sys.argv or '-trace' in sys.argv:
+        log.Handler = log_to_file
+        config_obj.LogHandler = log_to_file
+
+    if '-trace' in sys.argv:
+        config_obj.LogHandler = log.level.TRACE
+
+    if '-dev' in sys.argv:
+        console_obj.run_mode = 'dev'
+
+    log.show_value(
+        'Main',
+        log.level.INFO,
+        '執行模式',
+        console_obj.run_mode
+    )
+
+    event_console = EventConsole()
+    console_obj.event = event_console
+
+    dynamic_data_obj = DynamicData(console_obj)
+    console_obj.dynamic_data = dynamic_data_obj
+
+    comm_obj = Command(console_obj)
+    console_obj.command = comm_obj
+
+    black_list = BlackList(console_obj)
+
+    feedback = Feedback(console_obj)
+    ptt_adapter = PTT_Adapter(console_obj)
+
+    # websocketserver 是特例
+    websocketserver.config = config_obj
+    websocketserver.command = comm_obj
+
+    run_server = True
+    def event_close():
+        global run_server
+        run_server = False
+
+    event_console.close.append(websocketserver.stop)
+    event_console.close.append(event_close)
+
+    websocketserver.start()
+
+    if websocketserver.start_error:
+        log.show(
+            'Main',
+            log.level.INFO,
+            'websocket server startup error'
+        )
+        for e in event_console.close:
+            e()
+    else:
+        while run_server:
+            try:
+                time.sleep(0.5)
+            except KeyboardInterrupt:
+                for e in event_console.close:
+                    e()
+                break
+
+    log.show(
+        'Main',
+        log.level.INFO,
+        '執行最終終止程序')
+
+    running = threading.Event()
+    running.set()
+    running.clear()
+
+    log.show(
+        'Main',
+        log.level.INFO,
+        '最終終止程序全數完成'
+    )
--- server/src/uPtt.py
+++ server/src/uPtt.py
@@ -1,130 +1,68 @@
-import os
-import sys
-import time
-import threading
+from sys import argv
+from os import startfile
+from io import BytesIO
+import zipfile
+from urllib.request import urlopen
+from pathlib import Path
 
 import log
-import websocketserver
 from config import Config
-from command import Command
-from pttadapter import PTT_Adapter
-from feedback import Feedback
-from event import EventConsole
 from console import Console
 from dynamic_data import DynamicData
-from black_list import BlackList
+from event import EventConsole
+import util
 
-LogPath = None
+config_obj = Config()
 
+console_obj = Console()
+console_obj.config = config_obj
 
-def log_to_file(msg):
-    global LogPath
-    if LogPath is None:
-        desktop = os.path.join(
-            os.path.join(
-                os.environ['USERPROFILE']
-            ),
-            'Desktop'
-        )
+event_console = EventConsole()
+console_obj.event = event_console
 
-        LogPath = f'{desktop}/uPttLog.txt'
+dynamic_data_obj = DynamicData(console_obj, run=False)
 
-        print(LogPath)
+log.show_value(
+    'uLauncher',
+    log.level.INFO,
+    'Local version',
+    config_obj.version)
 
-    with open(LogPath, 'a', encoding='utf8') as f:
-        f.write(f'{msg}\n')
+log.show_value(
+    'uLauncher',
+    log.level.INFO,
+    'Dynamic version',
+    dynamic_data_obj.data['version'])
 
+version_local = config_obj.version
+version_dynamic = dynamic_data_obj.data['version']
 
-if __name__ == '__main__':
+version_compare_result = util.compare_version(version_local, version_dynamic)
+file_exist = Path(f'{config_obj.config_path}/uPtt.exe').is_file()
 
-    config_obj = Config()
-
-    console_obj = Console()
-    console_obj.config = config_obj
-
-    log.show_value(
-        'Main',
-        log.level.INFO,
-        'uPtt 版本',
-        config_obj.version
-    )
-
-    if len(sys.argv) > 1:
-        print(sys.argv)
-
-    if '-debug' in sys.argv or '-trace' in sys.argv:
-        log.Handler = log_to_file
-        config_obj.LogHandler = log_to_file
-
-    if '-trace' in sys.argv:
-        config_obj.LogHandler = log.level.TRACE
-
-    if '-dev' in sys.argv:
-        console_obj.run_mode = 'dev'
-
-    log.show_value(
-        'Main',
-        log.level.INFO,
-        '執行模式',
-        console_obj.run_mode
-    )
-
-    event_console = EventConsole()
-    console_obj.event = event_console
-
-    dynamic_data_obj = DynamicData(console_obj)
-    console_obj.dynamic_data = dynamic_data_obj
-
-    comm_obj = Command(console_obj)
-    console_obj.command = comm_obj
-
-    black_list = BlackList(console_obj)
-
-    feedback = Feedback(console_obj)
-    ptt_adapter = PTT_Adapter(console_obj)
-
-    # websocketserver 是特例
-    websocketserver.config = config_obj
-    websocketserver.command = comm_obj
-
-    run_server = True
-    def event_close():
-        global run_server
-        run_server = False
-
-    event_console.close.append(websocketserver.stop)
-    event_console.close.append(event_close)
-
-    websocketserver.start()
-
-    if websocketserver.start_error:
-        log.show(
-            'Main',
-            log.level.INFO,
-            'websocket server startup error'
-        )
-        for e in event_console.close:
-            e()
-    else:
-        while run_server:
-            try:
-                time.sleep(0.5)
-            except KeyboardInterrupt:
-                for e in event_console.close:
-                    e()
-                break
-
+if version_compare_result < 0 or not file_exist:
+    url = 'https://github.com/PttCodingMan/uPtt/raw/develop/server/package/uPtt.zip'
     log.show(
-        'Main',
+        'uLauncher',
         log.level.INFO,
-        '執行最終終止程序')
-
-    running = threading.Event()
-    running.set()
-    running.clear()
-
+        '開始更新')
+    resp = urlopen(url)
     log.show(
-        'Main',
+        'uLauncher',
         log.level.INFO,
-        '最終終止程序全數完成'
-    )
+        '更新完成')
+    zip_file = zipfile.ZipFile(BytesIO(resp.read()))
+    zip_file.extractall(path=config_obj.config_path)
+
+a = ''
+if len(argv) > 1:
+    print(argv)
+    a = ' ' + ' '.join(argv[1:])
+
+    process = argv.copy()
+    process[0] = f'{config_obj.config_path}/uPtt.exe'
+
+import subprocess
+subprocess.Popen(process)
+
+# startfile(f'{config_obj.config_path}/uPtt.exe{a}')
